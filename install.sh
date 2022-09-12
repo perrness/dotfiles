2200f09353adc5d247dfab6ebdd9939ae491f17c
Per Næss <perness3009@gmail.com>
2022-09-12 22:13:41 +0200

    chore: update install file

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
 cp starship.toml ~/.config/starship.toml
-cp -R nvim ~/.config/nvim
+cp -R nvim ~/.config
+cp -R fish ~/.config
+cp -R alacritty ~/.config
 
 echo "Installation Complete"
