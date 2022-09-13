beec719eea71b406c964d0eff560d42761d05339
Per Næss <per-richard.naess@gjensidige.no>
2022-09-13 08:51:54 +0200

    feat: disable fish greeting

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,13 @@ alias kx="kubectx"
 alias ls="exa --icons"
 alias ll="exa --group --header --group-directories-first --long --icons --git"
 
+# SSH stuff
+eval (ssh-agent -c)
+ssh-add --apple-use-keychain --apple-load-keychain  ~/.ssh/id_ed25519-2
+
+# Fish settings
+set fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
