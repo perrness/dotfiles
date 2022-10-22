70c9b7d5ff86308ae49e6471cd70dae4c4cca689
Per Næss <perness3009@gmail.com>
2022-10-22 12:09:11 +0200

    fix(color): add integrations to catppuccin

--- nvim/lua/per/color.lua
+++ nvim/lua/per/color.lua
@@ -1,3 +1,10 @@
 require("catppuccin").setup({
   transparent_background = true,
+  integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		telescope = true,
+		treesitter = true,
+  },
 })
