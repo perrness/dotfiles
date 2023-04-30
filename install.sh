ac588796ccae65c7038fb31220cea1cdf631a51a
Per Næss <perness3009@gmail.com>
2023-04-30 19:59:54 +0200

    fix: update script

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp starship.toml ~/.config/starship.toml
-cp -R nvim ~/.config
-cp -R fish ~/.config
-cp -R alacritty ~/.config
-cp -R .local/bin ~/.local/bin
+cp -Rv nvim ~/.config
+cp -Rv fish ~/.config
+cp -Rv alacritty ~/.config
+cp -Rv .local/bin ~/.local/
 
 echo "Installation Complete"
