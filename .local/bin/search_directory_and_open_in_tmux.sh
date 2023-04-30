499e8b1a666af2389055c48268a63d0c620b5c7d
Per Næss <perness3009@gmail.com>
2023-04-30 17:44:09 +0200

    feat: create new tmux session from within

--- .local/bin/search_directory_and_open_in_tmux.sh
+++ .local/bin/search_directory_and_open_in_tmux.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
-directory=$(find ~/repoes -type d -print -maxdepth 1 | fzf)
-session_name=$(echo $directory | sed -e 's/.*\///g')
-tmux new -s $session_name -c $directory
+find_directory() {
+  directory=$(find ~/repoes -type d -print -maxdepth 1 | fzf)
+  return_code="$?"
+  if [ $return_code -ne 0 ]; then
+    exit $return_code
+  fi
+  session_name=$(echo $directory | sed -e 's/.*\///g')
+}
+
+start_tmux_session() {
+  if [ ! $TMUX ]; then
+    tmux new -s $session_name -c $directory -d
+    tmux attach -t $session_name
+  else
+    tmux new -s $session_name -c $directory -d
+    tmux switch-client -t $session_name
+  fi
+}
+
+find_directory
+start_tmux_session
