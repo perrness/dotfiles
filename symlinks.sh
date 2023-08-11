871390b41eaca6ea4078473f5b5d2bce180acb2f
Per Næss <per-richard.naess@gjensidige.no>
2023-08-11 10:46:58 +0200

    feat: add .vimrc to symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -5,5 +5,6 @@ ln -sf ~/repos/dotfiles/starship.toml ~/.config/starship.toml
 ln -sf ~/repos/dotfiles/nvim ~/.config
 ln -sf ~/repos/dotfiles/fish ~/.config
 ln -sf ~/repos/dotfiles/alacritty ~/.config
+ln -sf ~/repos/dotfiles/.vimrc ~/.vimrc
 
 echo "Symlinks Complete"
