85fb40c23ac7c167c276f2c577152e8d2f294ee0
Per Næss <per-richard.naess@gjensidige.no>
2024-04-02 08:58:07 +0200

    feat: add copilot plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,25 +1,26 @@
 return {
-   {
+  {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   },
   'nvim-tree/nvim-web-devicons',
-   'hrsh7th/nvim-cmp',         -- Autocompletion plugin
-   'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
-   'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-   'L3MON4D3/LuaSnip',         -- Snippets plugin
-   'lewis6991/gitsigns.nvim',
-   'simrat39/rust-tools.nvim',
-   {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  'hrsh7th/nvim-cmp',         -- Autocompletion plugin
+  'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
+  'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+  'L3MON4D3/LuaSnip',         -- Snippets plugin
+  'lewis6991/gitsigns.nvim',
+  'simrat39/rust-tools.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   },
-   'windwp/nvim-autopairs',
-   { "catppuccin/nvim", as = "catppuccin" },
-   { "ellisonleao/gruvbox.nvim" },
-   'freddiehaddad/feline.nvim',
-   'google/vim-jsonnet',
-   {
+  'windwp/nvim-autopairs',
+  { "catppuccin/nvim",         as = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+  'freddiehaddad/feline.nvim',
+  'google/vim-jsonnet',
+  {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
@@ -28,5 +29,6 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
-}
+  },
+  'github/copilot.vim',
 }
