4cbf220502301764d2cae4c39acdf28f863c65bf
Per Næss <per-richard.naess@gjensidige.no>
2024-09-17 12:55:28 +0200

    feat: update ts lsp config

--- nvim/lua/per/lsp.lua
+++ nvim/lua/per/lsp.lua
@@ -68,7 +68,7 @@ require('lspconfig')['terraformls'].setup {
   capabilities = capabilities,
 }
 
-require('lspconfig')['tsserver'].setup {
+require('lspconfig')['ts_ls'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
